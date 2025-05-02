2ae0e1fda8d0bf0e1e0af6e35ee43af7925d67ee
Aa <aa@example.com>
2025-05-02 17:50:02 +0300

    small fixes

---
+++ src/auth/inputs/register.input.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class RegisterInput {
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(80)
+  name: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(128)
+  password: string;
+}
