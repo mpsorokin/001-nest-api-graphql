959c40f086055dac937917c7ca1c23224edbfbd2
Aa <aa@example.com>
2025-05-02 16:22:14 +0300

    small fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -12,9 +12,11 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
+      imports: [ConfigModule],
       useFactory: getGraphQLConfig,
+      inject: [ConfigService],
     }),
     PrismaModule,
     AuthModule,
--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -1,10 +1,15 @@
-import { GqlModuleOptions } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { isDev } from '../utils/is-dev.util';
+import { ConfigService } from '@nestjs/config';
 
-export async function getGraphQLConfig(): Promise<GqlModuleOptions> {
+export async function getGraphQLConfig(
+  configService: ConfigService,
+): Promise<ApolloDriverConfig> {
   return {
     driver: ApolloDriver,
-    autoSchemaFile: true,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     sortSchema: true,
+    playground: isDev(configService),
   };
 }
