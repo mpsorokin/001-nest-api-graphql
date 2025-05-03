36b2e63a5ad13116523b66d5abf595f794f993ad
Aa <aa@example.com>
2025-05-03 14:51:49 +0300

    small fixes

--- src/auth/guards/authorized.guard.ts
+++ src/auth/guards/authorized.guard.ts
@@ -5,9 +5,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const Authorized = createParamDecorator(
   (data: keyof User, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-
     const request = ctx.getContext().req;
-
     const user = request.user as User;
 
     return data ? user[data] : user;
---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { User, UserRole } from '../../../generated/prisma';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const rolesContext = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!rolesContext) return true;
+
+    const ctx = GqlExecutionContext.create(context);
+    const request = ctx.getContext().req;
+    const user = request.user as User;
+
+    if (!rolesContext.includes(user.role))
+      throw new ForbiddenException('Not enough rules');
+
+    return true;
+  }
+}
