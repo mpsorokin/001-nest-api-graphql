aa5bec67ecb9315fa94d1d815a61cf3d598b9dce
Aa <aa@example.com>
2025-05-03 13:59:03 +0300

    small fixes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { GQLContext } from '../common/interfaces/gql-context.interface';
 import { AuthModel } from './models/auth.model';
 import { RegisterInput } from './inputs/register.input';
+import { LoginInput } from './inputs/login.input';
 
 @Resolver()
 export class AuthResolver {
@@ -14,5 +15,20 @@ export class AuthResolver {
     @Args('data') input: RegisterInput,
   ) {
     return this.authService.register(res, input);
+  }
+
+  @Mutation(() => AuthModel)
+  async login(@Context() { res }: GQLContext, @Args('data') input: LoginInput) {
+    return this.authService.login(res, input);
+  }
+
+  @Mutation(() => AuthModel)
+  async refresh(@Context() { req, res }: GQLContext) {
+    return this.authService.refresh(req, res);
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Context() { res }: GQLContext) {
+    return this.authService.logout(res);
   }
 }
